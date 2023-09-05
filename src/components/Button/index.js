3a11d8e376e69b170956e46b433b66ac2347c144
zidoopal <yuzzzin@gmail.com>
2023-09-05 12:04:49 +0900

    Button 기능 구현 완료

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React from 'react';
+import { StyledBtn } from './styles';
 
+// ButtonContainer Component
 const Button = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1>Button</h1>
+      <div>
+        <StyledBtn
+          size="large"
+          border="type1"
+          onClick={() => alert('버튼을 만들어보세요')}
+        >
+          Large Primary
+        </StyledBtn>
+        <StyledBtn size="medium" theme="type1">
+          Medium
+        </StyledBtn>
+        <StyledBtn size="small" theme="type1">
+          type1
+        </StyledBtn>
+        <StyledBtn
+          size="large"
+          border="type2"
+          onClick={() => prompt('어렵나요?')}
+        >
+          Large Negative
+        </StyledBtn>
+        <StyledBtn size="medium" theme="type2">
+          Medium
+        </StyledBtn>
+        <StyledBtn size="small" theme="type2">
+          Small
+        </StyledBtn>
+      </div>
+    </>
+  );
 };
 
 export default Button;
