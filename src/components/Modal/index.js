f775092027af7be023a003f3d48159a355c03899
zidoopal <yuzzzin@gmail.com>
2023-09-06 14:56:15 +0900

    Modal 기능 구현 완료

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,7 +1,85 @@
-import React from "react";
+import React from 'react';
+import { useState } from 'react';
+import { StyledBtn } from '../Button/styles';
+import { ModalContainer, StyledModal } from '../Modal/styles';
 
-const Input = () => {
-  return <div></div>;
+const Modal = () => {
+  const [OpenSmallBtnModal, setOpenSmallBtnModal] = useState(false);
+  const [OpenLargeBtnModal, setOpenLargeBtnModal] = useState(false);
+
+  return (
+    <>
+      <h1>Modal</h1>
+      <div>
+        <StyledBtn
+          size="small"
+          theme="type1"
+          onClick={() => setOpenSmallBtnModal(!OpenSmallBtnModal)}
+        >
+          open modal
+        </StyledBtn>
+        <StyledBtn
+          size="large"
+          theme="type2"
+          onClick={() => setOpenLargeBtnModal(!OpenLargeBtnModal)}
+        >
+          open modal
+        </StyledBtn>
+      </div>
+      <div>
+        {OpenSmallBtnModal && (
+          <>
+            <ModalContainer>
+              <StyledModal>
+                <p>
+                  닫기와 확인 버튼 2개가 있고,
+                  <br />
+                  외부 영역을 눌러도 모달이 닫히지 않아요.
+                </p>
+                <div className="btn-set">
+                  <StyledBtn
+                    size="small"
+                    theme="type2"
+                    onClick={() => setOpenSmallBtnModal(!OpenSmallBtnModal)}
+                  >
+                    닫기
+                  </StyledBtn>
+                  <StyledBtn
+                    size="small"
+                    theme="type1"
+                    onClick={() => console.log('on!')}
+                  >
+                    확인
+                  </StyledBtn>
+                </div>
+              </StyledModal>
+            </ModalContainer>
+          </>
+        )}
+        {OpenLargeBtnModal && (
+          <>
+            <ModalContainer
+              onClick={() => setOpenLargeBtnModal(!OpenLargeBtnModal)}
+            >
+              <StyledModal>
+                <div className="modal-bg"></div>
+                <p>
+                  닫기 버튼 1개가 있고,
+                  <br />
+                  외부 영역을 누르면 모달이 닫혀요.
+                </p>
+                <StyledBtn
+                  onClick={() => setOpenLargeBtnModal(!OpenLargeBtnModal)}
+                >
+                  X
+                </StyledBtn>
+              </StyledModal>
+            </ModalContainer>
+          </>
+        )}
+      </div>
+    </>
+  );
 };
 
-export default Input;
+export default Modal;
