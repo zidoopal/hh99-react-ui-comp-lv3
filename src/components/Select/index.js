4d402c16f08e610ab26bef16535d4ea966280816
zidoopal <yuzzzin@gmail.com>
2023-09-06 14:55:55 +0900

    Select 기능 구현 완료

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,7 +1,71 @@
-import React from "react";
+import { React } from 'react';
+import { useState } from 'react';
+import { IoMdArrowDropdown } from 'react-icons/io';
+import {
+  SelectContainer,
+  SelctBarSet,
+  Selectbar,
+  Label,
+  Dropdown,
+  OptionList,
+  IconWrap,
+} from './styles';
 
-const Input = () => {
-  return <div></div>;
+const CustomSelct = ({ options }) => {
+  const [isShowOptions, setShowOptions] = useState(false);
+  const [currentValue, setCurrentValue] = useState(options[0].value);
+
+  const handleOnChangeSelect = (e) => {
+    setCurrentValue(e.target.innerText);
+    // 리스트 선택 후 닫힘
+    setShowOptions(false);
+  };
+
+  return (
+    <>
+      <Selectbar onClick={() => setShowOptions(!isShowOptions)}>
+        <Label>{currentValue}</Label>
+        <IconWrap>
+          <IoMdArrowDropdown size="20" color="white" />
+        </IconWrap>
+        <Dropdown show={isShowOptions}>
+          {options.map((item) => (
+            <OptionList
+              key={item.id}
+              value={item.value}
+              show={isShowOptions}
+              onClick={handleOnChangeSelect}
+            >
+              {item.value}
+            </OptionList>
+          ))}
+        </Dropdown>
+      </Selectbar>
+    </>
+  );
 };
 
-export default Input;
+const Select = () => {
+  const options = [
+    { id: 1, value: '리액트' },
+    { id: 2, value: '자바' },
+    { id: 3, value: '스프링' },
+    { id: 4, value: '리액트 네이티브' },
+  ];
+
+  return (
+    <>
+      <h1>Select</h1>
+      <SelectContainer>
+        <SelctBarSet>
+          <CustomSelct options={options} />
+          <div className="hidden">
+            <CustomSelct options={options} />
+          </div>
+        </SelctBarSet>
+      </SelectContainer>
+    </>
+  );
+};
+
+export default Select;
