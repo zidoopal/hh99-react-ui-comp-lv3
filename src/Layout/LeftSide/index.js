39ae5ac76a981e5a96ceef3ed2f70869ad5a7a07
zidoopal <yuzzzin@gmail.com>
2023-09-07 14:45:47 +0900

    LeftSide js 파일 수정

--- src/Layout/LeftSide/index.js
+++ src/Layout/LeftSide/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import Button from '../../components/Button/index';
-import Modal from '../../components/Modal/index';
 import { LeftSideContainer, TransitionDiv } from './styles';
+import { StyledBtn } from '../../components/Button/styles';
+import { ModalContainer } from '../../components/Modal/styles';
 
-const LeftSide = () => {
+const ModalBtn = ({ size, theme, onClick }) => (
+  <StyledBtn size={size} theme={theme} onClick={onClick} className="modal-btn">
+    open
+  </StyledBtn>
+);
+
+const LeftSide = ({ toggleSmallModal, toggleLargeModal }) => {
   return (
     <LeftSideContainer>
       <TransitionDiv>
         <Button />
       </TransitionDiv>
+
       <TransitionDiv>
-        <Modal />
+        <ModalContainer>
+          <h2>Modal</h2>
+          <ModalBtn size="small" theme="type1" onClick={toggleSmallModal}>
+            open
+          </ModalBtn>
+
+          <ModalBtn size="large" theme="type2" onClick={toggleLargeModal}>
+            open
+          </ModalBtn>
+        </ModalContainer>
       </TransitionDiv>
     </LeftSideContainer>
   );
