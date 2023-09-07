1c21663b506efd640f9d520d668fccef7d47f509
zidoopal <yuzzzin@gmail.com>
2023-09-07 10:16:08 +0900

    App.jsx 파일 등록

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Reset } from 'styled-reset';
+import GlobalStyles from './GlobalStyles';
+import Header from './Layout/Header/index';
+import LeftSide from './Layout/LeftSide/index';
+import RightSide from './Layout/RightSide/index';
+import styled from 'styled-components';
 
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <MainContainer>
+        <Reset />
+        <GlobalStyles />
+        <LeftSide />
+        <RightSide />
+      </MainContainer>
+    </>
+  );
 };
+
+const MainContainer = styled.div`
+  width: 800px;
+  height: 840px;
+  display: flex;
+  justify-content: space-evenly;
+  margin: 0px auto;
+`;
 
 export default App;
