af6f652fd7b68d112535a06a599067c5e9bdc822
jin <thseokjin@naver.com>
2023-09-08 01:21:49 +0900

    파일 제출

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Buttons from "./components/Buttons";
+import Input from "./components/Input";
+import Modals from "./components/Modals";
+import Select from "./components/Select";
 
-const App = () => {
-  return <></>;
-};
+function App() {
+  return (
+    <>
+      <Buttons />
+      <Input />
+      <Modals />
+      <Select />
+    </>
+  );
+}
 
 export default App;
