c5d42fbccbe7d1c92db3d9aef41737b87b4e826d
zidoopal <yuzzzin@gmail.com>
2023-09-07 10:45:37 +0900

    Button index.js 수정

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,40 +1,45 @@
 import React from 'react';
-import { StyledBtn } from './styles';
+import { StyledBtn, ButtonContainer } from './styles';
 
-// ButtonContainer Component
 const Button = () => {
   return (
-    <>
-      <h1>Button</h1>
+    <ButtonContainer>
+      <h2>Button</h2>
       <div>
-        <StyledBtn
-          size="large"
-          border="type1"
-          onClick={() => alert('버튼을 만들어보세요')}
-        >
-          Large Primary
-        </StyledBtn>
-        <StyledBtn size="medium" theme="type1">
-          Medium
-        </StyledBtn>
-        <StyledBtn size="small" theme="type1">
-          type1
-        </StyledBtn>
-        <StyledBtn
-          size="large"
-          border="type2"
-          onClick={() => prompt('어렵나요?')}
-        >
-          Large Negative
-        </StyledBtn>
-        <StyledBtn size="medium" theme="type2">
-          Medium
-        </StyledBtn>
-        <StyledBtn size="small" theme="type2">
-          Small
-        </StyledBtn>
+        <div className="primary-btn-set">
+          <StyledBtn
+            size="large"
+            border="type1"
+            theme="type3"
+            onClick={() => alert('버튼을 만들어보세요')}
+          >
+            Large Primary
+          </StyledBtn>
+          <StyledBtn size="medium" theme="type1">
+            Medium
+          </StyledBtn>
+          <StyledBtn size="small" theme="type1">
+            Small
+          </StyledBtn>
+        </div>
+
+        <div className="negative-btn-set">
+          <StyledBtn
+            size="large"
+            border="type2"
+            onClick={() => prompt('어렵나요?')}
+          >
+            Large Negative
+          </StyledBtn>
+          <StyledBtn size="medium" theme="type2">
+            Medium
+          </StyledBtn>
+          <StyledBtn size="small" theme="type2">
+            Small
+          </StyledBtn>
+        </div>
       </div>
-    </>
+    </ButtonContainer>
   );
 };
 
