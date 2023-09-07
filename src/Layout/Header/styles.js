7c40ee8fdedf203dc3a3c6d690dc1ec1aa82db09
zidoopal <yuzzzin@gmail.com>
2023-09-07 09:00:13 +0900

    Header 컴포넌트 추가, CSS 스타일링 완료

---
+++ src/Layout/Header/styles.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const StyledHeader = styled.div`
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+  .react-logo {
+    display: inline-block;
+    position: relative;
+    right: 11px;
+    width: 25px;
+    height: 25px;
+    background-size: cover;
+    background-position: center center;
+
+    animation-name: spin;
+    animation-duration: 5000ms;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    transform-origin: 50% 50%;
+    margin: 0 -10px;
+  }
+
+  h1 {
+    color: white;
+    font-size: 2.4rem;
+    text-align: center;
+    margin-top: 30px;
+    letter-spacing: 1.2px;
+  }
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
